74406b6b4a8cf6cd18998933e23d5f139184231f
MOHAMED  ASARUDHEEN <masarudheena@gmail.com>
2018-03-17 00:09:30 +0530

    selector_commit

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -6,6 +6,7 @@ import numpy as np
 from hmmlearn.hmm import GaussianHMM
 from sklearn.model_selection import KFold
 from asl_utils import combine_sequences
+import array
 
 
 class ModelSelector(object):
@@ -68,16 +69,38 @@ class SelectorBIC(ModelSelector):
     Bayesian information criteria: BIC = -2 * logL + p * logN
     """
 
+    def calculateBIC(self, compNumber):
+        """
+        TODO
+        Calculate the average log likelihood of cross-validation folds using the KFold class
+        :return: tuple of the mean likelihood and the model with the respective score
+        """
+        BICModel = self.base_model(compNumber)
+        logL = BICModel.score(self.X, self.lengths)
+        p = compNumber * (compNumber - 1) + (compNumber - 1) + 2 * self.X.shape[1] * compNumber
+        BICscore = (-2 * logL) + (p * np.log(self.X.shape[0]))
+        return BICscore
+
     def select(self):
         """ select the best model for self.this_word based on
-        BIC score for n between self.min_n_components and self.max_n_components
-
+        CV score for n between self.min_n_components and self.max_n_components
+        It is based on log likehood
         :return: GaussianHMM object
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        # TODO implement model selection based on DIC scores
+        bScore = -1
+        BICModel = None
+        bestBICModel = None
+        for compNumber in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                bicScore = self.calculateBIC(compNumber)
+                if bicScore > bScore:
+                    bScore = bicScore
+                    bestBICModel = self.base_model(compNumber)
+            except Exception as e:
+                continue
+        return bestBICModel
 
 
 class SelectorDIC(ModelSelector):
@@ -90,11 +113,44 @@ class SelectorDIC(ModelSelector):
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
 
-    def select(self):
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
+    def calculateDIC(self, compNumber):
+        """
+        TODO
+        Calculate the average log likelihood of cross-validation folds using the KFold class
+        :return: tuple of the mean likelihood and the model with the respective score
+        """
+        DICModel = self.base_model(compNumber)
+        logL = DICModel.score(self.X, self.lengths)
+        total_logL = 0
+        for word in self.words:
+            word_x, word_x_lengths = self.hwords[word]
+            total_logL += DICModel.score(word_x, word_x_lengths)
+        avg_logL = total_logL / (len(self.words) - 1)
+        dic_score = logL - avg_logL
 
+        return dic_score
+
+    def select(self):
+        """ select the best model for self.this_word based on
+        CV score for n between self.min_n_components and self.max_n_components
+        It is based on log likehood
+        :return: GaussianHMM object
+        """
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
         # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        bScore = float("Inf")
+        bestDICModel = None
+        for compNumber in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                dic_score = self.calculateDIC(compNumber)
+                print(dic_score)
+                if dic_score < bScore:
+                    bScore = dic_score
+                    bestDICModel = self.base_model(compNumber)
+            except Exception as e:
+                print(e)
+                continue
+        return bestDICModel
 
 
 class SelectorCV(ModelSelector):
@@ -102,45 +158,42 @@ class SelectorCV(ModelSelector):
 
     '''
 
+    def findScore(self, compNumber):
+        """
+        Calculate the average log likelihood of cross-validation folds using the KFold class
+        :return: tuple of the mean likelihood and the model with the respective score
+        """
+        scores = []
+        kf= KFold(n_splits=2)
+
+        for cvTrainIndex, cvTestIndex in kf.split(self.sequences):
+            self.X, self.lengths = combine_sequences(cvTrainIndex, self.sequences)
+            cvTestX, cvTestLen = combine_sequences(cvTestIndex, self.sequences)
+            hmmModel = self.base_model(num_states=compNumber).fit(self.X, self.lengths)
+
+            scores.append(hmmModel.score(cvTestX, cvTestLen))
+
+        return np.mean(scores), hmmModel
+
     def select(self):
+        """ select the best model for self.this_word based on
+        CV score for n between self.min_n_components and self.max_n_components
+        It is based on log likehood
+        :return: GaussianHMM object
+        """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
-        bestModelNumber = []
-        bestScore = -1
-        Mean = -2
+
+        bScore = float("Inf")
+        bHmmModel = None
         for compNumber in range(self.min_n_components, self.max_n_components + 1):
-            scoreList = []
+            try:
+                crrScore, model = self.findScore(compNumber)
+                if crrScore < bScore:
+                    best_score = crrScore
+                    bHmmModel = model
+            except Exception as e:
+                continue
+        return bHmmModel
 
-            words = self.sequences
-            totalWords = len(words)
-            log_likelihoods = []
-            splitDataNum = 0
-            if totalWords>2:
-                splitDataNum = 3
-                kf = KFold(n_splits=splitDataNum)
-                for cvTrainIndex, cvTestIndex in kf.split(self.sequences):
-                    cvTrainX,cvTrainLen = combine_sequences(cvTrainIndex,self.sequences)
-                    cvTestX, cvTestLen = combine_sequences(cvTestIndex, self.sequences)
-                    hmmModel  = self.base_model(num_states = compNumber).fit(cvTrainX,cvTrainLen)
-                    scoreList = hmmModel.score(cvTestX,cvTestLen)
-                    #scoreList.append(score)
 
-            else:
-                kf = KFold(n_splits=2)
-                for cvTrainIndex, cvTestIndex in kf.split(self.sequences):
-                    cvTrainX,cvTrainLen = combine_sequences(cvTrainIndex,self.sequences)
-                    cvTestX, cvTestLen = combine_sequences(cvTestIndex, self.sequences)
-                    hmmModel  = self.base_model(num_states = compNumber).fit(cvTrainX,cvTrainLen)
-                    scoreList = hmmModel.score(cvTestX,cvTestLen)
-                    #scoreList.append(score)
-            Mean = np.mean(scoreList)
-
-            if Mean>bestScore:
-                print(bestScore,"adfds")
-                bestScore = Mean
-                bestModelNumber.append(compNumber)
-
-        for i in bestModelNumber:
-            print(i)
-        return self.base_model(1)
